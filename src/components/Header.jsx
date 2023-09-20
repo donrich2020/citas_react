633af54d0551a6f668a6988ce9d48fe45575639f
r2kjet@hotmail.com <r2kjet@hotmail.com>
2023-09-20 17:47:15 -0500

    Arreglos pequeños y cambio de título

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
     
     return (
         <>
-            <h1 className="text-4xl font-black uppercase text-center md:w-1/2 mx-auto">Seguimiento Pacientes <span className="text-indigo-600">Veterinaria</span></h1>
+            <h1 className="text-4xl font-black uppercase text-center md:w-1/2 mx-auto">Seguimiento Pacientes <span className="text-indigo-600">Rich Pets</span></h1>
         </>
     )
 }
